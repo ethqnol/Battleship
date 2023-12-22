dd0343d968f8d06aa0e27aefaa34141756f73f3e
ethqnol <112029223+ethqnol@users.noreply.github.com>
2023-12-22 12:44:03 -0500

    asdfasdf

---
+++ src/routes/demo/+page.svelte
@@ -0,0 +1,554 @@
+
+
+<script lang="ts">
+    import type { Ship } from "../../interfaces/ship";
+    import GridCell from "../../components/Cell.svelte";
+    import { afterUpdate } from "svelte";
+    import OwnGridCell from "../../components/OwnCell.svelte";
+    import type { Cell } from "../../interfaces/cell";
+    import { Type } from "../../interfaces/type";
+    import type { Position } from "../../interfaces/position";
+    let GRIDWIDTH = 10;
+    let computerWin = false;
+    let started = false;
+    let moves = 0;
+    let playerShips : Ship[] = [
+        { type: Type.carrier, size: 5, hits: [], location: [], sunk: false },
+        { type: Type.battleship, size: 4, hits: [], location: [], sunk: false  },
+        { type: Type.cruiser, size: 3, hits: [], location: [], sunk: false  },
+        { type: Type.destroyer, size: 3, hits: [], location: [], sunk: false  },
+        { type: Type.submarine, size: 2, hits: [], location: [], sunk: false  },
+    ];
+
+    let defaultCell : Cell = { ship: Type.none, hit: false, score: 0, sunk: false };
+    let yourGrid = Array.from({length: GRIDWIDTH}, e => Array(GRIDWIDTH).fill(defaultCell));
+    let computerProbabilityMap = Array.from({length: GRIDWIDTH}, e => Array(GRIDWIDTH).fill(defaultCell));
+
+    function reset() {
+      computerWin = false;
+      moves = 0;
+      playerShips = [
+        { type: Type.carrier, size: 5, hits: [], location: [], sunk: false },
+        { type: Type.battleship, size: 4, hits: [], location: [], sunk: false  },
+        { type: Type.cruiser, size: 3, hits: [], location: [], sunk: false  },
+        { type: Type.destroyer, size: 3, hits: [], location: [], sunk: false  },
+        { type: Type.submarine, size: 2, hits: [], location: [], sunk: false  },
+    ];
+
+
+
+      let defaultCell : Cell = { ship: Type.none, hit: false, score: 0, sunk: false };
+      yourGrid = Array.from({length: GRIDWIDTH}, e => Array(GRIDWIDTH).fill(defaultCell));
+      computerProbabilityMap = Array.from({length: GRIDWIDTH}, e => Array(GRIDWIDTH).fill(defaultCell));
+    }
+
+
+    function computerPossibleCoords(grid : Cell[][], ship : Ship, orientation : string) {
+      let possibleCoords : Position[] = [];
+      if(orientation == "up-down"){
+        for(let x = 0; x < GRIDWIDTH; x += 1){
+          for(let y = 0; y < GRIDWIDTH - ship.size; y += 1){
+            
+            let okay : boolean = true;
+            if(y > 0){
+              if( grid[x][y - 1].ship != Type.none){
+                okay = false;
+              }
+            }
+            if(y + ship.size < GRIDWIDTH){
+              if(grid[x][y + ship.size].ship != Type.none){
+                okay = false;
+              }
+            }
+
+            if(okay){
+              for(let i = y; i < y + ship.size; i += 1 ){
+                if(grid[x][i].ship != Type.none){
+                    okay = false;
+                    break;
+                }
+                if(x > 0){
+                  if(grid[x-1][i].ship != Type.none){
+                    okay = false;
+                    break;
+                  }
+                }
+
+                if(x < GRIDWIDTH - 1 ){
+                  if(grid[x+1][i].ship != Type.none){
+                    okay = false;
+                    break;
+                  }
+                }
+              }
+            }
+          
+            if(okay) {
+              possibleCoords.push({ col: x, row: y})
+            }
+
+          }
+          
+        }
+      } else {
+        for(let x = 0; x <= GRIDWIDTH - ship.size; x += 1){
+          for(let y = 0; y < GRIDWIDTH; y += 1){
+            let okay : boolean = true;
+            if(x > 0){
+              if(grid[x - 1][y].ship != Type.none){
+                 okay = false;
+              }
+            }
+
+            if(x + ship.size < GRIDWIDTH){
+              if(grid[x + ship.size][y].ship != Type.none){
+                 okay = false;
+              }
+            }
+
+            if(okay){
+              for(let i = x; i < x + ship.size; i += 1 ){
+                if(grid[i][y].ship != Type.none){
+                  okay = false;
+                  break;
+                }
+                if(y > 0){
+                  if( grid[i][y-1].ship != Type.none){
+                    okay = false;
+                    break;
+                  }
+                }
+               
+                if(y < GRIDWIDTH - 1){
+                  if(grid[i][y+1].ship != Type.none){
+                    okay = false;
+                    break;
+                  }
+                }
+              }
+            }
+            if(okay) {
+              possibleCoords.push({ col: x, row: y})
+            }
+
+          }
+          
+        }
+      }
+
+
+      return possibleCoords;
+    }
+
+    function placeShips(grid : Cell[][], ships : Ship[]){
+      for(let i = 0; i < ships.length; i += 1){
+        let orientation : string = "up-down"
+        if(Math.random() > 0.5){
+          orientation = "left-right"
+        }
+
+        let possibleCoords : Position[] = computerPossibleCoords(grid, ships[i], orientation);
+        let finalCoord = possibleCoords[Math.floor(Math.random() * possibleCoords.length)]
+        possibleCoords = []
+        if(orientation == "left-right"){
+          let y = finalCoord.row;
+          for(let x = finalCoord.col; x < finalCoord.col + ships[i].size; x += 1){
+            let newPosition : Position = {
+              row: y,
+              col: x
+            }
+            
+            grid[x][y] = { ship: ships[i].type, hit: false, score: 0, sunk: false};
+
+            ships[i].location.push(newPosition);
+          }
+        } else {  
+          let x = finalCoord.col
+          for(let y = finalCoord.row; y < finalCoord.row + ships[i].size; y += 1){
+            let newPosition : Position = {
+              row: y,
+              col: x
+            }
+            
+            grid[x][y] = { ship: ships[i].type, hit: false, score: 0, sunk: false};
+
+            ships[i].location.push(newPosition);
+          }
+        }
+      }
+    }
+
+    
+   
+    function start() {
+      reset();
+      started = true;
+      placeShips(yourGrid, playerShips)
+      setTimeout(function() {
+        playComputerMove()}, 400)
+    }
+
+
+    function evalProbabilityMap() {
+      for (let x = 0; x < GRIDWIDTH; x++) {
+        for (let y = 0; y < GRIDWIDTH; y++) {
+          computerProbabilityMap[x][y].score = 0;
+        }
+      }
+      for (let x = 0; x < GRIDWIDTH; x++) {
+        for (let y = 0; y < GRIDWIDTH; y++) {
+          let score = 0;
+          for (let i = 0; i < playerShips.length; i++) {
+            if(playerShips[i].sunk == true && (playerShips[i].size == 2 || playerShips[i].size == 3)){
+              continue;
+            } else {
+              let shipSize = playerShips[i].size
+
+              for(let index = x - shipSize + 1; index <= x; index ++){
+                if(index + shipSize - 1 > GRIDWIDTH - 1){
+                  break;
+                }
+                if(index >= 0){
+                  let fits = true;
+                  for(let j = 0; j < shipSize; j++){
+                    if(computerProbabilityMap[index + j][y].hit == true){
+                      fits = false;
+                      break;
+                    }
+
+                  }
+                  if(fits) score += 1;
+                }
+              }
+              
+              for(let index = y - shipSize + 1; index <= y; index ++){
+                if(index + shipSize - 1 > GRIDWIDTH - 1){
+                  break;
+                }
+                if(index >= 0){
+                  let fits = true;
+                  for(let j = 0; j < shipSize; j++){
+                    if(computerProbabilityMap[x][index + j].hit == true){
+                      fits = false;
+                      break;
+                    }
+
+                  }
+                  if(fits) score += 1;
+                }
+              }
+            }  
+            
+            if(computerProbabilityMap[x][y].hit == true && computerProbabilityMap[x][y].sunk == false && computerProbabilityMap[x][y].ship != Type.none){
+             
+             if(x+1 < GRIDWIDTH) {
+              if(computerProbabilityMap[x+1][y].hit == false){
+               if(x-1 >= 0 && computerProbabilityMap[x-1][y].sunk == false && computerProbabilityMap[x-1][y].hit == true && computerProbabilityMap[x-1][y].ship != Type.none){
+                 computerProbabilityMap[x+1][y] = { ship: computerProbabilityMap[x+1][y].ship, hit: computerProbabilityMap[x+1][y].hit, score: computerProbabilityMap[x+1][y].score + 25, sunk: computerProbabilityMap[x+1][y].sunk}
+               } else {
+                 computerProbabilityMap[x+1][y] = { ship: computerProbabilityMap[x+1][y].ship, hit: computerProbabilityMap[x+1][y].hit, score: computerProbabilityMap[x+1][y].score + 15, sunk: computerProbabilityMap[x+1][y].sunk}
+               }
+              }
+             }
+             if(x-1 >= 0) {
+              if(computerProbabilityMap[x-1][y].hit == false){
+               if(x+1 < GRIDWIDTH && computerProbabilityMap[x+1][y].sunk == false && computerProbabilityMap[x+1][y].hit == true && computerProbabilityMap[x+1][y].ship != Type.none){
+                 computerProbabilityMap[x-1][y] = { ship: computerProbabilityMap[x-1][y].ship, hit: computerProbabilityMap[x-1][y].hit, score: computerProbabilityMap[x-1][y].score + 25, sunk: computerProbabilityMap[x-1][y].sunk}
+               } else {
+                 computerProbabilityMap[x-1][y] = { ship: computerProbabilityMap[x-1][y].ship, hit: computerProbabilityMap[x-1][y].hit, score: computerProbabilityMap[x-1][y].score + 15, sunk: computerProbabilityMap[x-1][y].sunk}
+               }
+              }
+             }
+             if(y+1 < GRIDWIDTH) {
+              if(computerProbabilityMap[x][y+1].hit == false){
+               if(y-1 >= 0 && computerProbabilityMap[x][y-1].sunk == false && computerProbabilityMap[x][y-1].hit == true && computerProbabilityMap[x][y-1].ship != Type.none){
+                 computerProbabilityMap[x][y+1] = { ship: computerProbabilityMap[x][y+1].ship, hit: computerProbabilityMap[x][y+1].hit, score: computerProbabilityMap[x][y+1].score + 25, sunk: computerProbabilityMap[x][y+1].sunk}
+               } else {
+                 computerProbabilityMap[x][y+1] = { ship: computerProbabilityMap[x][y+1].ship, hit: computerProbabilityMap[x][y+1].hit, score: computerProbabilityMap[x][y+1].score + 15, sunk: computerProbabilityMap[x][y+1].sunk}
+               }
+              }
+             }
+             if(y-1 >= 0) {
+              if(computerProbabilityMap[x][y-1].hit == false){
+                if(y+1 < GRIDWIDTH && computerProbabilityMap[x][y+1].sunk == false &&  computerProbabilityMap[x][y+1].hit == true &&  computerProbabilityMap[x][y+1].ship != Type.none){
+                  computerProbabilityMap[x][y-1] = { ship: computerProbabilityMap[x][y-1].ship, hit: computerProbabilityMap[x][y-1].hit, score: computerProbabilityMap[x][y-1].score + 25, sunk: computerProbabilityMap[x][y-1].sunk}
+                } else {
+                  computerProbabilityMap[x][y-1] = { ship: computerProbabilityMap[x][y-1].ship, hit: computerProbabilityMap[x][y-1].hit, score: computerProbabilityMap[x][y-1].score + 15, sunk: computerProbabilityMap[x][y-1].sunk}
+                }
+              }
+            }
+           } else if(computerProbabilityMap[x][y].hit == true){
+             computerProbabilityMap[x][y] = { ship: computerProbabilityMap[x][y].ship, hit: true, score: 0, sunk: computerProbabilityMap[x][y].sunk}
+           }
+          }
+
+          computerProbabilityMap[x][y] = { ship: computerProbabilityMap[x][y].ship, hit: computerProbabilityMap[x][y].hit, score: computerProbabilityMap[x][y].score+score, sunk: computerProbabilityMap[x][y].sunk}
+        }
+      }
+
+    }
+
+    
+    function findHighestProbabilityCell() {
+      let highestScore = 0;
+      let bestTarget = { col: 0, row: 0 };
+
+      let direction = Math.floor(Math.random() * 4);
+
+      if(direction == 0){
+        for (let x = 0; x < GRIDWIDTH; x += 1) {
+          for (let y = 0; y < GRIDWIDTH; y += 1) {
+            if (computerProbabilityMap[x][y].score > highestScore) {
+              highestScore = computerProbabilityMap[x][y].score;
+              bestTarget = { col: x, row: y };
+            }
+          }
+        }
+      } else if (direction == 1){
+        for (let x = GRIDWIDTH - 1; x >= 0; x -= 1) {
+          for (let y = 0; y < GRIDWIDTH; y += 1) {
+            if (computerProbabilityMap[x][y].score > highestScore) {
+              highestScore = computerProbabilityMap[x][y].score;
+              bestTarget = { col: x, row: y };
+            }
+          }
+        }
+      } else if (direction == 2){
+        for (let x = 0; x < GRIDWIDTH; x += 1) {
+          for (let y = GRIDWIDTH- 1; y >= 0; y -= 1) {
+            if (computerProbabilityMap[x][y].score > highestScore) {
+              highestScore = computerProbabilityMap[x][y].score;
+              bestTarget = { col: x, row: y };
+            }
+          }
+        }
+      } else {
+        for (let x = GRIDWIDTH - 1; x >= 0; x -= 1) {
+          for (let y = GRIDWIDTH - 1; y >= 0; y -= 1) {
+            if (computerProbabilityMap[x][y].score > highestScore) {
+              highestScore = computerProbabilityMap[x][y].score;
+              bestTarget = { col: x, row: y };
+            }
+          }
+        }
+      }
+      return bestTarget;
+    }
+
+
+    function playComputerMove() {
+
+      evalProbabilityMap()
+       
+      let bestTarget = findHighestProbabilityCell();
+      let computerTarget = yourGrid[bestTarget.col][bestTarget.row];
+      yourGrid[bestTarget.col][bestTarget.row] = { ship: computerTarget.ship, hit: true, score: 0, sunk: false}
+      computerProbabilityMap[bestTarget.col][bestTarget.row] = { ship: computerTarget.ship, hit: true, score: 0, sunk: false}
+      
+      if(yourGrid[bestTarget.col][bestTarget.row].ship != Type.none){
+        let hitShip = playerShips.find(ship => ship.type == yourGrid[bestTarget.col][bestTarget.row].ship)
+        hitShip?.hits.push({col: bestTarget.col, row: bestTarget.row})
+        console.log(hitShip)
+        if(hitShip && hitShip?.hits.length == hitShip?.size){
+          hitShip.sunk = true;
+          hitShip.hits.map((pos : Position) => {
+            yourGrid[pos.col][pos.row] = { ship: yourGrid[pos.col][pos.row].ship, hit: true, score: 0, sunk: true}
+            computerProbabilityMap[pos.col][pos.row] = { ship: yourGrid[pos.col][pos.row].ship, hit: true, score: 0, sunk: true}
+          })
+          if(!playerShips.find(ship => ship.sunk == false)){
+            computerWin = true;
+            return;
+          }
+        }
+        moves += 1
+        setTimeout(function() {
+        playComputerMove()}, 400)
+        
+      } else {
+        moves += 1
+        setTimeout(function() {
+        playComputerMove()}, 400)
+      }
+
+      
+      
+      return;
+    }
+    let options: number[] = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+    
+    
+
+</script>
+
+
+<main>
+
+    <h2>Board:</h2>
+    
+
+    <div class="setting">
+        <label for="grid-size"> Grid Size: </label>
+        <select id="grid-size" bind:value={GRIDWIDTH} on:change={reset}>
+            
+            {#each options as value}<option {value}>{value}</option>{/each}
+        </select>
+    </div>
+    <h1> # Of Moves: {moves}</h1>
+
+    <button on:click={start}> Begin Simulation </button>
+      <div class="board">
+        
+        <div class="grid">
+          
+          <div class="row">
+            <div class="coords">{" "}</div>
+            {#each yourGrid[0] as _, j}
+              <div class="coords">{j}</div>
+            {/each}
+          </div>
+
+          <div class="column">
+            {#each yourGrid as column, i}
+              <div class="row">
+                <div class= "coords">{i}</div> 
+                {#each column as cell}
+                  <OwnGridCell {cell} />
+                {/each}
+              </div>
+            {/each}
+          </div>
+        </div>
+      </div>
+
+</main>
+  
+
+
+<style>
+    main {
+      margin: 0.5rem;
+    }
+
+
+    .board {
+        margin-top: 5rem;
+      display: flex;
+      flex-direction: column;
+      background-color: whitesmoke;
+      padding: 0.75rem;
+      outline: black solid 2px;
+      border-radius: 5px;
+    }
+    .grid {
+        
+      outline: 1px solid black;
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+    }
+    .column{
+        display:flex;
+        flex-direction: row;
+        gap: 1px;
+    }
+
+    .row{
+        display:flex;
+        flex-direction: column;
+        gap: 1px;
+    }
+
+    .coords{
+      height: 40px;
+        width: 40px;
+      background-color: #3a3a3a;
+      outline: 0.5px solid rgb(106, 106, 106);
+      text-align: center;
+      vertical-align: middle;
+      line-height: 50px;
+      color: white
+    }
+
+    
+    select{
+      padding: 5px;
+      border-radius: 2px;
+    }
+    .start-button {
+      color: white;
+      font-size: 1rem;
+      font-weight: 900;
+      padding: 5px;
+      border: none;
+      border-radius: 5px;
+      background-color: blue;
+    }
+
+    .start-button:hover {
+      color: white;
+      font-size: 1rem;
+      font-weight: 900;
+      padding: 5px;
+      border: none;
+      border-radius: 5px;
+      background-color: rgb(18, 18, 125);
+      cursor: pointer
+    }
+
+    h1 {
+        
+      font-size: 1.4rem;
+      padding: 10px;
+      font-family: monospace;
+      font-weight: 900;
+    }
+
+    h2{
+      font-family:'Courier New', Courier, monospace;
+    }
+
+    h4{
+      font-family:'Courier New', Courier, monospace;
+      text-align: center;
+      font-size: 1.8rem;
+      color: white;
+      margin: 0px;
+      padding: 15px;
+      font-weight: 900;
+      border-radius: 4px;
+    }
+
+
+
+    button {
+      color: white;
+      font-size: 1rem;
+      font-weight: 900;
+      padding: 5px;
+      border: none;
+      border-radius: 5px;
+      background-color: blue;
+    }
+
+    button:hover {
+      color: white;
+      font-size: 1rem;
+      font-weight: 900;
+      padding: 5px;
+      border: none;
+      border-radius: 5px;
+      background-color: rgb(18, 18, 125);
+      cursor: pointer
+    }
+
+    .setting {
+        margin-top: 1rem;
+    }
+
+    label {
+        font-size: 1.0rem;
+        font-family: monospace;
+    }
+</style>
+ 
